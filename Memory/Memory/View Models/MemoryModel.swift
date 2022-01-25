6ec1493b0a2a0221accd528922e17b43cc185e0d
Nader Alfares <nna5040@psu.edu>
2022-01-25 13:44:30 -0500

    comleted the model in Class

--- Memory/Memory/View Models/MemoryModel.swift
+++ Memory/Memory/View Models/MemoryModel.swift
@@ -8,6 +8,63 @@
 import Foundation
 
 struct MemoryModel {
+    var sequence : [Int] = []
+    var currentGuessIndex = 0
+    var preferences = Preferences.standard
+    
+    enum GameState {
+        case notPlaying, memorizing, guessing, lost, won
+    }
+    
+    var gameState : GameState = .notPlaying
+    
+    mutating func newGame(){
+        sequence.removeAll()
+        gameState = .memorizing
+        currentGuessIndex = 0
+        
+        for _ in (0..<preferences.sequenceLength){
+            let randomIndex = Int.random(in: 0..<GameConstants.numberOfChoices)
+            sequence.append(randomIndex)
+        }
+    }
+    
+    mutating func incrementGuessIndex(){
+        self.currentGuessIndex += 1
+    }
+    
+    mutating func checkGuess(guess:Int){
+        let isCorrect = (guess == sequence[currentGuessIndex])
+        let isDone    = (currentGuessIndex == sequence.count - 1)
+        
+        if isCorrect{
+            if isDone{
+                gameState = .won
+            } else {
+                incrementGuessIndex()
+            }
+            
+        }else {
+            gameState = .lost
+        }
+    
+    }
+    
+    mutating func advanceGameState() {
+        switch gameState {
+        case .notPlaying:
+            gameState = .memorizing
+        case .memorizing:
+            gameState = .guessing
+        case .lost, .won:
+            gameState = .memorizing
+        default:
+                assert(false, "Button should be disabled while guessing")
+        }
+        
+    }
+    
+    
     
     
 }
